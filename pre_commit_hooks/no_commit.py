53bbc8d9d55c8fc4dbaf9d9d9ffc76b92154b1c2
Alexander Rieß <alexander.riess@icinga.com>
2024-12-17 22:48:58 +0100

    added the functionality

--- pre_commit_hooks/no_commit.py
+++ pre_commit_hooks/no_commit.py
@@ -1,3 +1,14 @@
+import sys
+
 def main():
-    print("Test")
-    exit(1)
+    patterns = ["no-commit", "nocommit", "no-checkin", "nocheckin"]
+    files = sys.argv[1:]
+    for file in files:
+        with open(file, 'r') as f:
+            content = f.read().lower()
+            for pattern in patterns:
+                if pattern in content:
+                    print(f"Pattern '%s' found in file: %s" % (pattern, file))
+                    exit(1)
+            
+    exit(0)
